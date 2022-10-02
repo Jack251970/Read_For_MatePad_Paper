ce8a6d8386ccc99f909a6eae26044bfd0fc4343d
Jack251970 <1160210343@qq.com>
2022-10-02 11:03:15 +0800

    ***2022/10/02*** * 删除了RecycleRefreshView下的加载动画

--- app/src/main/java/com/jack/bookshelf/help/ItemTouchCallback.java
+++ app/src/main/java/com/jack/bookshelf/help/ItemTouchCallback.java
@@ -116,7 +116,6 @@ public class ItemTouchCallback extends ItemTouchHelper.Callback {
 
     /**
      * 当Item被拖拽的时候被回调
-     *
      * @param recyclerView     recyclerView
      * @param srcViewHolder    拖拽的ViewHolder
      * @param targetViewHolder 目的地的viewHolder
@@ -151,14 +150,12 @@ public class ItemTouchCallback extends ItemTouchHelper.Callback {
     public interface OnItemTouchCallbackListener {
         /**
          * 当某个Item被滑动删除的时候
-         *
          * @param adapterPosition item的position
          */
         void onSwiped(int adapterPosition);
 
         /**
          * 当两个Item位置互换的时候被回调
-         *
          * @param srcPosition    拖拽的item的position
          * @param targetPosition 目的地的Item的position
          * @return 开发者处理了操作应该返回true，开发者没有处理就返回false
